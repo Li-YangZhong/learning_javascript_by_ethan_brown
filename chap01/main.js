ef4919603d0d7be4e5ef67cc6fb5b0b3bad2287e
Eric Li <liyangzhong@hotmail.com>
2020-03-09 17:28:10 +0800

    update

--- chap01/main.js
+++ chap01/main.js
@@ -11,11 +11,19 @@ $(document).ready(function () {
             }
         } */
 
-/*     var tool = new Tool();
-    tool.onMouseDown = function (event) {
-        var c = Shape.Circle(event.point.x, event.point.y, 20);
-        c.fillColor = 'green';
-    }; */
+    /*     var tool = new Tool();
+        tool.onMouseDown = function (event) {
+            var c = Shape.Circle(event.point.x, event.point.y, 20);
+            c.fillColor = 'green';
+        }; */
+
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
 
     var tool = new Tool();
     tool.onMouseDown = function (event) {
@@ -23,7 +31,7 @@ $(document).ready(function () {
         c.fillColor = 'green';
     };
 
-    
+
 
     paper.view.draw();
 });
